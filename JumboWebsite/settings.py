2a1b1e80f7ec86d14cd3cb72af7142b438c378bd
jarno <supergerrit@users.noreply.github.com>
2023-03-15 10:03:58 +0100

    Revert "Updated to Python 3.10"
    This reverts commit 074a5d1e0b6c07e7540575e94f33dbfda278ef03.

--- JumboWebsite/settings.py
+++ JumboWebsite/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 LOCAL = False
 
 ALLOWED_HOSTS = [os.getenv("SERVER_HOST"), 'localhost', '127.0.0.1']
